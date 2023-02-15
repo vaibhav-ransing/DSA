a072e936dd5c7c6f92823aed4110cb723058e979
vaibhav.ransing <vaibhavransing999@gmail.com>
2023-02-16 00:09:20 +0530

    Codes Tree

--- Archit/test.java
+++ Archit/test.java
@@ -2,34 +2,8 @@ import java.util.Stack;
 
 public class test {
 
-    public boolean isValid(String s) {
-        // { [ (
-
-        Stack<Character> stack = new Stack<>();
-        for(char ch: s.toCharArray()){
-            if(ch==')'){
-                if(stack.size()==0 || stack.peek()!='('){
-                    return false;
-                }else{
-                    stack.pop();
-                }
-            }else if(ch=='}'){
-                if(stack.size()==0 || stack.peek()!='{'){
-                    return false;
-                }else{
-                    stack.pop();
-                }
-            }else if(ch==']'){
-                if(stack.size()==0 || stack.peek()!='['){
-                    return false;
-                }else{
-                    stack.pop();
-                }
-            }else{
-                stack.push(ch);
-            }
-        }
-        return stack.size()==0;
+    public int removeDuplicates(int[] nums) {
+        Ha
     }
 
 
