9a99d16cc1200fdb39e26fa0992818565ef1e591
vaibhav.ransing <vaibhavransing999@gmail.com>
2023-06-06 14:12:46 +0530

    codes

--- Archit/ChatGpt/Code.java
+++ Archit/ChatGpt/Code.java
@@ -1,56 +1,28 @@
-import javax.persistence.*;
-
-@Entity
-@Table(name = "data_api")
-public class DataApi {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "sr_no")
-    private Long srNo;
-
-    @Column(name = "icto", nullable = false)
-    private String icto;
-
-    @Column(name = "api_type", nullable = false)
-    private String apiType;
-
-    @Column(name = "created_at", nullable = false)
-    private LocalDateTime createdAt;
-
-    // Constructors, getters, and setters
-}
-
-// -------------------------------
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DataApiRepository extends JpaRepository<DataApi, Long> {
-}
-
-// ------------------------------
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Service
-public class YourService {
+@RestController
+public class JenkinsApiController {
 
     private final DataApiRepository dataApiRepository;
 
     @Autowired
-    public YourService(DataApiRepository dataApiRepository) {
+    public JenkinsApiController(DataApiRepository dataApiRepository) {
         this.dataApiRepository = dataApiRepository;
     }
 
-    public void saveDataToTable(String icto, String apiType) {
+    @PostMapping("/jenkinsApi")
+    public ResponseEntity<String> processJenkinsData(@RequestBody JenkinsModel jenkinsModel) {
         DataApi dataApi = new DataApi();
-        dataApi.setIcto(icto);
-        dataApi.setApiType(apiType);
+        dataApi.setIcto(jenkinsModel.getIcto());
+        dataApi.setApiType(jenkinsModel.getName());
         dataApi.setCreatedAt(LocalDateTime.now());
 
         dataApiRepository.save(dataApi);
+
+        return ResponseEntity.ok("Data saved successfully");
     }
 }
-
-
-
