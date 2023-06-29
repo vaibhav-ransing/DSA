9b9133e9bf1794c5717d5084aeaa3776bb8af3cc
vaibhav.ransing <vaibhavransing999@gmail.com>
2023-06-29 20:03:40 +0530

    Codes

--- Archit/ChatGpt/Application/test.py
+++ Archit/ChatGpt/Application/test.py
@@ -1,7 +1,18 @@
-import json
+import requests
 
-# Read JSON from file
-with open("data.json", "r") as file:
-    json_data = json.load(file)
-    
-print(json_data)
+def fetch_issues(user_id, user_password, component_name):
+    url = f"https://next.sonarqube.com/sonarqube/web_api/api/issues/search?componentKeys={component_name}"
+    response = requests.get(url, auth=(user_id, user_password))
+    if response.status_code == 200:
+        json_data = response.json()
+        # Process the JSON response
+        # ...
+    else:
+        print(f"Request failed with status code: {response.status_code}")
+
+# Example usage
+user_id = "your_user_id"
+user_password = "your_user_password"
+component_name = "your_component_name"
+
+fetch_issues(user_id, user_password, component_name)
