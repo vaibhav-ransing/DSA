806cc1a1e2913befc693156b4e785fdb1471df8d
vaibhav.ransing <vaibhavransing999@gmail.com>
2023-06-29 20:15:24 +0530

    Codes

--- Archit/ChatGpt/Application/test.py
+++ Archit/ChatGpt/Application/test.py
@@ -1,14 +1,13 @@
-import requests
+import subprocess
+import json
 
 def fetch_issues(user_id, user_password, component_name):
-    url = f"https://next.sonarqube.com/sonarqube/web_api/api/issues/search?componentKeys={component_name}"
-    response = requests.get(url, auth=(user_id, user_password))
-    if response.status_code == 200:
-        json_data = response.json()
-        # Process the JSON response
-        # ...
-    else:
-        print(f"Request failed with status code: {response.status_code}")
+    curl_command = f'curl -k -X GET -u {user_id}:{user_password} "https://next.sonarqube.com/sonarqube/web_api/api/issues/search?componentKeys={component_name}"'
+    output = subprocess.check_output(curl_command, shell=True)
+    json_data = json.loads(output)
+
+    # Process the JSON response
+    # ...
 
 # Example usage
 user_id = "your_user_id"
