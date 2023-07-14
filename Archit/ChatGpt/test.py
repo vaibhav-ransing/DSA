b236f179684695b23b381d38611b24fe9b150039
vaibhav.ransing <vaibhavransing999@gmail.com>
2023-07-14 16:56:10 +0530

    Popup

--- Archit/ChatGpt/test.py
+++ Archit/ChatGpt/test.py
@@ -1,8 +1,40 @@
-import threading
-
 def show_confirmation_popup(message, user_id, user_password, component_name, folder_location, branch_name):
-    response = messagebox.askquestion("Confirmation", message, icon="warning")
-    if response == "yes":
+    # Create the confirmation popup window
+    confirmation_popup = tk.Toplevel(window)
+    confirmation_popup.title("Confirmation")
+    confirmation_popup.configure(bg="#F8F8F2")  # Light background color
+    confirmation_popup.attributes("-topmost", True)
+
+    # Center the confirmation popup window
+    confirmation_popup_width = 300
+    confirmation_popup_height = 200
+    confirmation_popup_x = window.winfo_x() + int((window_width - confirmation_popup_width) / 2)
+    confirmation_popup_y = window.winfo_y() + int((window_height - confirmation_popup_height) / 2)
+    confirmation_popup.geometry(f"{confirmation_popup_width}x{confirmation_popup_height}+{confirmation_popup_x}+{confirmation_popup_y}")
+
+    # Set custom font and size for the confirmation popup window
+    confirmation_popup_custom_font = ("Arial", 12)
+
+    # Create a frame for the content
+    content_frame = tk.Frame(confirmation_popup, bg="#F8F8F2")
+    content_frame.pack(pady=10)
+
+    # Split the message into multiple lines
+    message_lines = message.split("\n")
+
+    # Create and arrange the labels with the message lines
+    for line in message_lines:
+        label_line = tk.Label(content_frame, text=line, bg="#F8F8F2", fg="#2E3440", font=confirmation_popup_custom_font)
+        label_line.pack(pady=1)
+
+    # Create and arrange the buttons with reduced size and spacing
+    button_frame = tk.Frame(confirmation_popup, bg="#F8F8F2")  # Custom background color
+    button_frame.pack(pady=10)
+
+    def on_yes():
+        # Close the confirmation popup
+        confirmation_popup.destroy()
+
         # Create the loading popup window
         loading_popup = tk.Toplevel(window)
         loading_popup.title("Loading")
@@ -26,7 +58,6 @@ def show_confirmation_popup(message, user_id, user_password, component_name, fol
         # Disable the main window
         window.grab_set()
 
-        # Run the sonar_script in a separate thread
         def run_sonar_script():
             try:
                 sonar_script(user_id, user_password, component_name, folder_location, branch_name)
@@ -37,13 +68,30 @@ def show_confirmation_popup(message, user_id, user_password, component_name, fol
                 loading_popup.destroy()
 
                 # Show the success popup
-                show_ok_popup("All the vulnerabilities are fixed")
+                show_ok_popup("All the vulnerabilities are fixed \n Please check the statistic report", False)
             except Exception as e:
-                messagebox.showerror("Error", str(e))
+                show_ok_popup("Please check your credentials", True)
 
         # Start a new thread for running the sonar_script
         thread = threading.Thread(target=run_sonar_script)
         thread.start()
-    else:
-        # If the user clicks "no", close the confirmation popup
-        messagebox.showinfo("Confirmation", "Action canceled")
+
+    def on_no():
+        # Close the confirmation popup
+        confirmation_popup.destroy()
+
+    button_yes = tk.Button(button_frame, text="Yes", command=on_yes, bg="#6272A4", fg="#F8F8F2", font=confirmation_popup_custom_font, relief=tk.FLAT, width=8, height=1)  # Custom colors, reduced size
+    button_yes.pack(side=tk.LEFT, padx=10)
+
+    button_no = tk.Button(button_frame, text="No", command=on_no, bg="#6272A4", fg="#F8F8F2", font=confirmation_popup_custom_font, relief=tk.FLAT, width=8, height=1)  # Custom colors, reduced size
+    button_no.pack(side=tk.LEFT, padx=10)
+
+    # Center the content frame within the confirmation popup window
+    content_frame.pack_configure(anchor=tk.CENTER)
+
+    # Bring the confirmation popup window to the front and focus it
+    confirmation_popup.lift()
+    confirmation_popup.focus_force()
+
+    # Make the main window inactive until the confirmation popup window is closed
+    window.wait_window(confirmation_popup)
