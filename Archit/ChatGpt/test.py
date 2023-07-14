37f6976832f9351e8509ab1e29894d8855e87d7e
vaibhav.ransing <vaibhavransing999@gmail.com>
2023-07-14 19:01:53 +0530

    Popup

--- Archit/ChatGpt/test.py
+++ Archit/ChatGpt/test.py
@@ -5,7 +5,7 @@ import threading
 def show_loading_popup(user_id, user_password, component_name, folder_location, branch_name):
     loading_popup = tk.Toplevel(window)
     loading_popup.title("Loading")
-    loading_popup.configure(bg=BG_COLOR)
+    loading_popup.configure(bg="#F8F8F2")  # Light background color
     loading_popup.attributes("-topmost", True)
 
     loading_popup_width = 300
@@ -17,15 +17,15 @@ def show_loading_popup(user_id, user_password, component_name, folder_location, 
     loading_custom_font = ("Arial", 14)
 
     # Create a frame for the content
-    content_frame = tk.Frame(loading_popup, bg=BG_COLOR)
+    content_frame = tk.Frame(loading_popup, bg="#F8F8F2")  # Custom background color
     content_frame.pack(pady=10)
 
     # Create and place the label with the loading message
-    label_loading = tk.Label(content_frame, text="Please wait, fixing vulnerabilities", bg=BG_COLOR, fg=FG_COLOR, font=loading_custom_font)
+    label_loading = tk.Label(content_frame, text="Please wait, fixing vulnerabilities", bg="#F8F8F2", fg="#2E3440", font=loading_custom_font)  # Custom colors
     label_loading.pack()
 
     # Create a frame for the progress bar
-    progress_frame = tk.Frame(loading_popup, bg=BG_COLOR)
+    progress_frame = tk.Frame(loading_popup, bg="#F8F8F2")  # Custom background color
     progress_frame.pack(pady=10)
 
     # Create a progress bar widget
@@ -62,3 +62,41 @@ def show_loading_popup(user_id, user_password, component_name, folder_location, 
 
 # Call the show_loading_popup function where appropriate in your code, based on your application's flow,
 # and provide the necessary arguments: user_id, user_password, component_name, folder_location, branch_name.
+
+
+
+def show_ok_popup(message, is_error):
+    ok_popup = tk.Toplevel(window)
+    ok_popup.title("Confirmation")
+    ok_popup.configure(bg="#F8F8F2")  # Light background color
+    ok_popup.attributes("-topmost", True)
+
+    ok_popup_width = 300
+    ok_popup_height = 150
+    ok_popup_x = window.winfo_x() + int((window_width - ok_popup_width) / 2)
+    ok_popup_y = window.winfo_y() + int((window_height - ok_popup_height) / 2)
+    ok_popup.geometry(f"{ok_popup_width}x{ok_popup_height}+{ok_popup_x}+{ok_popup_y}")
+
+    ok_custom_font = ("Arial", 12)
+
+    # Determine the background color based on error or success
+    bg_color = "#FF0000" if is_error else "#00FF00"
+
+    # Create a frame for the content
+    content_frame = tk.Frame(ok_popup, bg=bg_color)  # Custom background color
+    content_frame.pack(pady=10)
+
+    # Create and place the label with the message
+    label_message = tk.Label(content_frame, text=message, bg=bg_color, fg="#2E3440", font=ok_custom_font)  # Custom colors
+    label_message.pack()
+
+    # Center the content frame within the popup window
+    content_frame.pack_configure(anchor=tk.CENTER)
+
+    # Bring the popup window to the front and focus it
+    ok_popup.lift()
+    ok_popup.focus_force()
+
+    # Make the main window inactive until the popup window is closed
+    window.wait_window(ok_popup)
+
