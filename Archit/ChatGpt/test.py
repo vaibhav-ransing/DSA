85868147558ebaa3b57101125561bdac05da42f2
vaibhav.ransing <vaibhavransing999@gmail.com>
2023-07-14 17:57:21 +0530

    Popup

--- Archit/ChatGpt/test.py
+++ Archit/ChatGpt/test.py
@@ -1,4 +1,8 @@
-def show_loading_popup(user_id, user_password, component_name, folder_location, branch_name):
+import tkinter as tk
+from tkinter import ttk
+import threading
+
+def show_loading_popup():
     loading_popup = tk.Toplevel(window)
     loading_popup.title("Loading")
     loading_popup.configure(bg=BG_COLOR)
@@ -12,24 +16,39 @@ def show_loading_popup(user_id, user_password, component_name, folder_location, 
 
     loading_custom_font = ("Arial", 14)
 
-    label_loading = tk.Label(loading_popup, text="Loading...", bg=BG_COLOR, fg=FG_COLOR, font=loading_custom_font)
-    label_loading.pack(pady=20)
+    # Create a frame for the content
+    content_frame = tk.Frame(loading_popup, bg=BG_COLOR)
+    content_frame.pack(pady=20)
 
+    # Create a progress bar widget
+    progress = ttk.Progressbar(content_frame, orient="horizontal", length=200, mode="indeterminate")
+    progress.pack()
+
+    # Start the progress bar animation
+    progress.start()
+
+    # Disable the main window
     window.attributes("-disabled", True)
-    window.update()
 
-    def run_sonar_script():
+    def simulate_loading():
         try:
             # Simulating a long-running task
-            sonar_script(user_id, user_password, component_name, folder_location, branch_name)
             import time
             time.sleep(5)
 
+            # Enable the main window
             window.attributes("-disabled", False)
-            loading_popup.destroy()
-            show_ok_popup("All the vulnerabilities are fixed\nPlease check the statistic report", False)
-        except Exception as e:
-            show_ok_popup("Please check your credentials", True)
 
-    thread = threading.Thread(target=run_sonar_script)
+            # Close the loading popup
+            loading_popup.destroy()
+
+            # Show a completion message
+            show_ok_popup("Task completed")
+        except Exception as e:
+            show_ok_popup("Error occurred", True)
+
+    # Start a new thread for simulating the loading
+    thread = threading.Thread(target=simulate_loading)
     thread.start()
+
+# Replace the previous show_loading_popup function with this updated version
