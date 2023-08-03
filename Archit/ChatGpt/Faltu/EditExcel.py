8a6dc7f25bc2389a79c9ed182fbc3dfab7d45ad3
vaibhav.ransing <vaibhavransing999@gmail.com>
2023-08-03 22:43:23 +0530

    Excel

--- Archit/ChatGpt/Faltu/EditExcel.py
+++ Archit/ChatGpt/Faltu/EditExcel.py
@@ -1,9 +1,10 @@
 import pandas as pd
+from openpyxl import load_workbook
 
 # Define the Excel file location
 report_location = 'your_excel_file.xlsx'
 
-# Load the Excel file
+# Load the existing Excel file
 xls = pd.ExcelFile(report_location)
 
 # Read the existing "Filtered_Data" sheet into a DataFrame
@@ -15,8 +16,11 @@ column_index = df_filtered.columns.get_loc('ColumnA')  # Get the index of the de
 new_value = 'UpdatedValue'
 df_filtered.iat[row_index, column_index] = new_value
 
-# Save the modified DataFrame back to the "Filtered_Data" sheet
-with pd.ExcelWriter(report_location, engine='openpyxl') as writer:
-    writer.book = xls.book
-    writer.sheets = dict((ws.title, ws) for ws in xls.book.worksheets)
+# Create a new ExcelWriter object with a copy of the existing workbook
+wb = load_workbook(filename=report_location)
+writer = pd.ExcelWriter(report_location, engine='openpyxl')
+writer.book = wb
+
+# Save the modified DataFrame back to the "Filtered_Data" sheet using the new ExcelWriter
+with writer.save():
     df_filtered.to_excel(writer, sheet_name='Filtered_Data', index=False)
