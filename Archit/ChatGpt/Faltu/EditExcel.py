6c7385c690b77129845e43df8a1804642bef0e24
vaibhav.ransing <vaibhavransing999@gmail.com>
2023-08-03 22:34:13 +0530

    Excel

--- Archit/ChatGpt/Faltu/EditExcel.py
+++ Archit/ChatGpt/Faltu/EditExcel.py
@@ -6,38 +6,17 @@ report_location = 'your_excel_file.xlsx'
 # Load the Excel file
 xls = pd.ExcelFile(report_location)
 
-# Read the "Filtered_Data" sheet into a DataFrame
+# Read the existing "Filtered_Data" sheet into a DataFrame
 df_filtered = pd.read_excel(xls, sheet_name='Filtered_Data')
 
-
-
-
-# Edit data in the DataFrame
+# Edit a specific cell using the iat method
 row_index = 0
-column_index = 0
+column_index = df_filtered.columns.get_loc('ColumnA')  # Get the index of the desired column
 new_value = 'UpdatedValue'
-df_filtered.at[row_index, column_index] = new_value
+df_filtered.iat[row_index, column_index] = new_value
 
-# Reopen the Excel file with the default "xlsxwriter" engine
-# with pd.ExcelWriter(report_location, engine='xlsxwriter') as writer:
-#     writer.book = xls.book
-#     writer.sheets = dict((ws.title, ws) for ws in xls.book.worksheets)
-
-#     # Write the modified DataFrame back to the "Filtered_Data" sheet
-#     df_filtered.to_excel(writer, sheet_name='Filtered_Data', index=False)
-
-# Save the modified DataFrame back to the "Filtered_Data" sheet with formatting
-with pd.ExcelWriter(report_location, engine='xlsxwriter') as writer:
-    writer.book = pd.load_workbook(report_location)
-    writer.sheets = dict((ws.title, ws) for ws in writer.book.worksheets)
-
-    # Write the DataFrame to the sheet
+# Save the modified DataFrame back to the "Filtered_Data" sheet
+with pd.ExcelWriter(report_location, engine='openpyxl') as writer:
+    writer.book = xls.book
+    writer.sheets = dict((ws.title, ws) for ws in xls.book.worksheets)
     df_filtered.to_excel(writer, sheet_name='Filtered_Data', index=False)
-
-    # Access the XlsxWriter workbook and worksheet objects
-    workbook = writer.book
-    worksheet = writer.sheets['Filtered_Data']
-
-    # Add cell formatting
-    cell_format = workbook.add_format({'bold': True, 'color': 'red'})
-    worksheet.write('A1', 'Updated', cell_format)
