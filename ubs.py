28b8a093b58b225a3c839ff4a36cb614864195f0
vaibhavransing999@gmail.com <vaibhavransing999@gmail.com>
2023-11-29 14:13:27 +0530

    update ubs.py

--- ubs.py
+++ ubs.py
@@ -1,23 +1,23 @@
 import os
 import re
 
-def extract_methods(folder):
-    method_pattern = re.compile(r'\s*def\s+(\w+)\(.*\):')  # Regex pattern for method extraction
+def extract_java_methods(folder):
+    method_pattern = re.compile(r'\s*(public|private|protected)?\s*\w+\s+(\w+)\s*\([^)]*\)\s*\{')  # Regex pattern for method extraction
 
     methods = []
     for root, _, files in os.walk(folder):
         for file_name in files:
-            if file_name.endswith('.py'):  # Assuming Python source files
+            if file_name.endswith('.java'):
                 file_path = os.path.join(root, file_name)
                 with open(file_path, 'r') as file:
                     content = file.read()
                     matches = method_pattern.findall(content)
-                    methods.extend(matches)
+                    methods.extend([match[1] for match in matches])
     return methods
 
 def find_untested_methods(src_folder, test_folder):
-    src_methods = extract_methods(src_folder)
-    test_methods = extract_methods(test_folder)
+    src_methods = extract_java_methods(src_folder)
+    test_methods = extract_java_methods(test_folder)
 
     untested_methods = [method for method in src_methods if method not in test_methods]
     return untested_methods
